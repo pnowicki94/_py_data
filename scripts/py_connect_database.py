a8cf5a15d5e17106cd225395801d4f07e99cfdd0
PN <pnowicki1994@gmail.com>
2022-08-23 13:10:29 +0200

    connect db

--- scripts/py_connect_database.py
+++ scripts/py_connect_database.py
@@ -1,9 +1,9 @@
 from database_connection.postgres import ConnectPostgres
-
+from database_connection.oracle import ConnectOracle
 """
 PostgreSQL
 """
-database_name = 'pg'
+database_name = 'test'
 schema_name = 'test'
 
 with ConnectPostgres(database_name, schema_name) as pg:
@@ -18,3 +18,13 @@ if pg.test_conn:
     cursor_pg.close()
 
 pg.close_connection()
+
+"""
+Oracle
+"""
+
+database_name = 'test'
+with ConnectOracle(database_name) as ora:
+    if ora.test_conn:
+        cursor_ora = ora.cursor
+        del cursor_ora
