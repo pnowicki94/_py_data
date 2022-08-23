6f0f80091ec42e30f0f6a68defbc5b01582a8a47
PN <pnowicki1994@gmail.com>
2022-08-23 13:10:58 +0200

    get_view_ddl

---
+++ tools/py_get_ddl_views_from_oracle.py
@@ -0,0 +1,39 @@
+from database_connection.oracle import ConnectOracle
+import os
+
+
+def get_view_ddl(database_name, _username, tables):
+    with ConnectOracle(database_name) as ora:
+        if ora.test_conn:
+            cursor = ora.cursor
+            views_ddl = ""
+            for table in tables:
+                print(table)
+
+                sql = f"""select dbms_metadata.get_ddl('VIEW', VIEW_NAME, '{_username}') from ALL_VIEWS WHERE 
+                            OWNER = '{_username}' and VIEW_NAME like '%{table}%'"""
+
+                try:
+                    cursor.execute(sql)
+                    view_ddl = str(cursor.fetchone()[0]) + ';\n'
+                except Exception as e:
+                    print(f'{table}: {e}')
+                    view_ddl = ""
+
+                views_ddl += view_ddl
+
+    return views_ddl
+
+
+if __name__ == '__main__':
+    views = [
+        'V_...'
+    ]
+
+    db = 'test'
+    username = 'test'
+    dir_files = r''
+    sql_ddl = get_view_ddl(db, username, views)
+
+    with open(dir_files + os.sep + 'sql_ddl_views.sql', 'w') as f:
+        f.write(sql_ddl)
